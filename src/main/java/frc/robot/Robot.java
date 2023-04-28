e43107fd20a4db92604f781bfc458655f42ebaa9
Moyu0730 <aslanchen0730@gmail.com>
2023-04-28 20:08:25 +0800

    Complete Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,7 @@
 
 package frc.robot;
 
-import java.util.List;
-
-import javax.naming.spi.DirStateFactory.Result;
+import java.util.concurrent.Delayed;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -28,15 +26,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends TimedRobot {
 
-  PhotonCamera m_PVCamera = new PhotonCamera("WEB_CAM");
-  boolean m_HasTarget;
-  PhotonPipelineResult m_Result;
-  PhotonTrackedTarget m_Target;
-  double m_X;
+  PhotonCamera mPVCamera = new PhotonCamera("Limelight1");
+  boolean mHasTarget;
+  PhotonPipelineResult mResult;
+  PhotonTrackedTarget mTarget;
+  double mX;
 
-  private Joystick m_js1 = new Joystick(0);
-  private DigitalOutput m_LeftLight = new DigitalOutput(1);
-  private DigitalOutput m_RightLight = new DigitalOutput(1);
+  private Joystick js1 = new Joystick(0);
+  private DigitalOutput m_LeftLight = new DigitalOutput(2);
+  private DigitalOutput m_RightLight = new DigitalOutput(4);
 
   // PhotonCamera camera = new PhotonCamera("WEB_CAM");
   // PhotonPipelineResult result;
@@ -46,6 +44,9 @@ public class Robot extends TimedRobot {
   // Define target
   // PhotonTrackedTarget target;
   // Transform3d bestCameraToTarget;
+
+  // Joystick
+  Joystick js = new Joystick(0);
 
   @Override
   public void robotInit() {}
@@ -58,36 +59,67 @@ public class Robot extends TimedRobot {
 
   /* This function is called periodically during autonomous. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    m_LeftLight.set(!true);
+    m_RightLight.set(!false);
+
+    // if( js1.getRawButton(1) ) LeftLight.set(false); 
+
+    // SmartDashboard.putBoolean("Button", js1.getRawButton(1));
+    SmartDashboard.putBoolean("LeftLight", !m_LeftLight.get());
+    SmartDashboard.putBoolean("RightLight", !m_RightLight.get());
+  }
 
   @Override
   public void autonomousInit() {
-    m_LeftLight.set(true);
-    m_RightLight.set(true);
-    m_X = 0;
+    m_LeftLight.set(!true);
+    m_RightLight.set(!true);
+    mX = 0;
   }
 
   @Override
   public void autonomousPeriodic() {
-    m_Result = m_PVCamera.getLatestResult();
-    m_HasTarget = m_Result.hasTargets();
+    // TODO_01
+    /*
+     *  宣告並獲取 Apriltag 要用的資料 
+     */
+    
+    // Example Code
+    //  result = ???;
+    //  hasTarget = ???;
+    mResult = mPVCamera.getLatestResult();
+    mHasTarget = mResult.hasTargets();
 
-    if( m_HasTarget ){
-      m_Target = m_Result.getBestTarget();
+    // TODO_02
+    /*
+     * PS 0. Task 順序不一定是對的喔，可能要先 2. 再 1. 也可能先 1. 再 2. 也可能要 1. 2. 同時
+     * Task 1. 輸出從鏡頭得到的資料
+     *  PS 1. 輸出資料所使用的函式
+     *    PS 1-1. 輸出鏡頭中是否有 Apriltag : SmartDashboard.putBoolean( ???, ??? );
+     *    PS 1-2. 輸出鏡頭跟 Apriltag 的距離 : SmartDashboard.putNumber( ???, ??? );
+     * Task 2. 思考可能會有問題的地方
+     *  PS 2. 提供一個資訊：如果鏡頭中沒有 Apriltag 但是你還跟他要資料的話整個程式都會掛掉，想一下要怎麼處理
+     */
+
+    if( mHasTarget ){
+      mTarget = mResult.getBestTarget();
       Transform3d mCameraOutput = getCameratoTarget();
-      m_X = mCameraOutput.getY();
+      mX = mCameraOutput.getY();
     }
 
-    if( m_X > 0.2 || m_X < -0.2 ){
-      m_LeftLight.set(false);
-      m_RightLight.set(false);
-    }else{
-      m_LeftLight.set(true);
-      m_RightLight.set(true);
+    if( mX < -0.2 ){
+      m_LeftLight.set(!true);
+      m_RightLight.set(!false);
+    }else if( mX > 0.2 ){
+      m_LeftLight.set(!false);
+      m_RightLight.set(!true);
+    }else{ 
+      m_LeftLight.set(!false);
+      m_RightLight.set(!false);
     }
 
-    SmartDashboard.putBoolean("HasTarget", m_HasTarget);
-    SmartDashboard.putNumber("X", m_X);
+    SmartDashboard.putBoolean("HasTarget", mHasTarget);
+    SmartDashboard.putNumber("X", mX);
     SmartDashboard.putBoolean("LeftLight", !m_LeftLight.get());
     SmartDashboard.putBoolean("RightLight", !m_RightLight.get());
   }
@@ -96,6 +128,6 @@ public class Robot extends TimedRobot {
    * Get the transform that maps camera ( X = forward, Y = left, Z = up ) to apriltag
    **/
   public Transform3d getCameratoTarget(){
-    return m_HasTarget == true ? m_Target.getBestCameraToTarget() : new Transform3d( new Translation3d(0,0,0), new Rotation3d(0,0,0) );
+    return mHasTarget == true ? mTarget.getBestCameraToTarget() : new Transform3d( new Translation3d(0,0,0), new Rotation3d(0,0,0) );
   }
 }
